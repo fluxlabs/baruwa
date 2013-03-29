8b562660c8e8f3ce51613253146737c76a068235
Anis <anis.jendoubi@gmail.com>
2013-03-29 21:41:11 +0100

    Update install.sh
    Update rabbitmq version

--- 2.0/cent6/install.sh
+++ 2.0/cent6/install.sh
@@ -56,23 +56,23 @@ timezone=America/Chicago
 # Information
 # +---------------------------------------------------+
 
-date="3-13-2013"						# Date
+date="3-29-2013"						# Date
 version="2.0"							# Version
-osver="Cent OS/RHEL x86_64"				# Script ID
+osver="Cent OS/RHEL x86_64"					# Script ID
 baruwa="2.0"							# Baruwa Version
 centalt="6-1"							# CenAlt Version
-epel="6-8"								# EPEL Version
+epel="6-8"							# EPEL Version
 rpmforge="0.5.2-2"						# RPM Forge Version
-rabbitmq="3.0.2"						# Rabbit MQ Version
+rabbitmq="3.0.4"						# Rabbit MQ Version
 msver="4.84.5-3"						# MailScanner Version
 msver1="4.84.5"							# MS Config Version
-libmem="1.0.15"                      	# LIB MEM Cache Version
+libmem="1.0.15"                     			 	# LIB MEM Cache Version
 pythonver="2.6"							# Python Version
 pyzorver="0.5.0"						# Pyzor Version
 home="/home/baruwa"						# Home Directory
-etcdir="/etc/baruwa"					# Baruwa etc
+etcdir="/etc/baruwa"						# Baruwa etc
 eximdir="/etc/exim"						# Exim Directory
-track="/tmp/tracking"					# Dir for tracking
+track="/tmp/tracking"						# Dir for tracking
 logs="/tmp/baruwa2"						# Dir for Logs
 
 # +---------------------------------------------------+
