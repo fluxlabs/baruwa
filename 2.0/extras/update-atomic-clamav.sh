d3a81cfd9c1f5480119706bd23e73e5e3e5878e9
Jeremy McSpadden <jeremy@fluxlabs.net>
2014-01-26 10:15:01 -0600

    Atomic SA Rules Script
    Added subscription based AtomicCorp SA Rules CronJob
    
    Signed-off-by: Jeremy McSpadden <jeremy@fluxlabs.net>

---
+++ 2.0/extras/update-atomic-clamav.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Atomic Clamav Rules for Centos/Baruwa Installer
+# Jeremy McSpadden
+# Flux Labs
+# jeremy at fluxlabs dot net
+# 01/26/14
+#
+# You must have a paid subscription for this to work.
+# You can sign-up for a trial at https://www.atomicorp.com/amember/cart/index/product/id/22/c/
+# For a subscription, sign up at https://www.atomicorp.com/amember/cart/index/product/id/29/c/
+# Just edit your username and password
+
+USER=username
+PASS=password
+
+# ==== NO NEED TO EDIT BELOW ===
+
+cd /usr/src
+wget --user=${USER} --password=${PASS} https://www.atomicorp.com/channels/rules/subscription/VERSION
+CLM=$(grep CLAMAV VERSION | awk -F = '{print $2}')
+
+cd /var/lib/clamav
+rm -rf ASL*
+cd /tmp
+wget --user=${USER} --password=${PASS} http://www.atomicorp.com/channels/rules/subscription/clamav-${CLM}.tar.gz
+tar zxvf clamav-*
+mv clamav/* /var/lib/clamav/.
+cd /var/lib/clamav
+chmod og+r ASL*
+rm -rf /tmp/clamav*
+chown clamav:
+/etc/init.d/clamd restart
+
+rm -f /usr/src/VERSION
