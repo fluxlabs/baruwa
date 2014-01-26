85999d8a484bb0eafd1f6fd9107dc86e7ec2df19
Jeremy McSpadden <jeremy@fluxlabs.net>
2014-01-26 10:19:52 -0600

    Compacted Code
    Signed-off-by: Jeremy McSpadden <jeremy@fluxlabs.net>

--- 2.0/extras/update-atomic-clamav.sh
+++ 2.0/extras/update-atomic-clamav.sh
@@ -8,7 +8,11 @@
 # You must have a paid subscription for this to work.
 # You can sign-up for a trial at https://www.atomicorp.com/amember/cart/index/product/id/22/c/
 # For a subscription, sign up at https://www.atomicorp.com/amember/cart/index/product/id/29/c/
+#
+# INSTALL GUIDE
+# cd /etc/cron.daily; wget https://raw.github.com/fluxlabs/baruwa/master/2.0/extras/update-atomic-clamav.sh
 # Just edit your username and password
+# Put this file in /etc/cron.daily
 
 USER=username
 PASS=password
@@ -19,16 +23,11 @@ cd /usr/src
 wget --user=${USER} --password=${PASS} https://www.atomicorp.com/channels/rules/subscription/VERSION
 CLM=$(grep CLAMAV VERSION | awk -F = '{print $2}')
 
-cd /var/lib/clamav
-rm -rf ASL*
-cd /tmp
-wget --user=${USER} --password=${PASS} http://www.atomicorp.com/channels/rules/subscription/clamav-${CLM}.tar.gz
-tar zxvf clamav-*
-mv clamav/* /var/lib/clamav/.
-cd /var/lib/clamav
-chmod og+r ASL*
-rm -rf /tmp/clamav*
-chown clamav:
+cd /var/lib/clamav; rm -rf ASL*
+cd /tmp; wget --user=${USER} --password=${PASS} http://www.atomicorp.com/channels/rules/subscription/clamav-${CLM}.tar.gz
+tar zxvf clamav-*; mv clamav/* /var/lib/clamav/.
+cd /var/lib/clamav; chmod og+r ASL*
+rm -rf /tmp/clamav*; chown clamav:
 /etc/init.d/clamd restart
 
 rm -f /usr/src/VERSION
