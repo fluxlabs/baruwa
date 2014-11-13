15ef5b52c78207a2cd5e94d77d4bab50fd514d18
Ash <TetraAsh@users.noreply.github.com>
2014-11-13 19:24:48 +0000

    Create unattend.sh

---
+++ extras/centos/unattend.sh
@@ -0,0 +1,57 @@
+# +---------------------------------------------------+
+# Automated Install
+# If you would like a completely automated install
+# Fill the required fields below.
+# +---------------------------------------------------+
+
+# Set 1 to Use the autocomplete. 0 to do prompts.
+useauto=0
+
+# Set 1 to pause after every step. (semi-debug?)
+usepause=0
+
+# Postgresql Password
+pssqlpass=passw0rd123!
+
+# RabbitMQ Password
+rabbpass=passw0rd321!
+
+# Baruwa Admin Email
+admemail=admin@domain.net
+
+# Baruwa Reports From Email
+repemail=reports@domain.net
+
+# Baruwa Error Reports From Email
+erremail=errors@domain.net
+
+# Baruwa URL
+baruwadomain=baruwa.domain.net
+
+# Baruwa Admin Username
+baruwaadmin=admin
+
+# Baruwa Admin Password - Must be Secure
+adminpass=Passw0rd123!
+
+# Baruwa Admin Email
+adminemail=admin@domain.net
+
+# Time Zone
+timezone=America/Chicago
+
+# MailScanner Organization Name - Long
+msorgnamelong='Your Organization'
+
+# MailScanner Organization Name - Short (No Spaces)
+msorgname='YourOrganization'
+
+# SSL Country Code
+sslcountry='US'
+
+# SSL Province/State Name
+sslprovince='Illinois'
+
+# SSL City Name
+sslcity='Chicago'
+# +=====================================================================+
