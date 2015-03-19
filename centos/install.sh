7270922427623dcb02af40bb1650a5c183f0a3b6
Ash <TetraAsh@users.noreply.github.com>
2015-03-19 11:50:25 +0000

    Freshclam.log chmod
    update to fix issue in https://github.com/fluxlabs/baruwa/issues/55

--- centos/install.sh
+++ centos/install.sh
@@ -1104,6 +1104,7 @@ f_clam (){
 		touch /var/log/clamav/freshclam.log
 		chown -R clamav: /var/log/clamav
 		chown -R clamav: /var/lib/clamav
+		chmod 664 /var/lib/clamav/freshclam.log
 		sleep 5
 		freshclam
 		service clamd start
